4c4ed2fcf9c801f28d5ce7bbc47eb201b885af5e
ziyad <ziyad.alnawfal1@gmail.com>
2025-03-14 06:09:06 +0000

    added for mysql database

--- back_end/mojave/main/models.py
+++ back_end/mojave/main/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 
-# Create your models here.
+# models for:
+
+# User
+# Most popular words
+# Category
+
+# The relationship between user and 'most popular words' and 'category' is many to many
 class User(models.Model):
-    username = models.CharField(max_length=25)
+    username = models.CharField(max_length=25,primary_key=True)
     password = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    location = models.CharField(max_length=255)
+    last_login = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=255) # allow if they allow
+    profile = models.CharField(max_length=50)
+
     def __str__(self):
         return self.username
+    
+class most_popular_words(models.Model):
+    word = models.CharField(max_length=50,primary_key=True)
+    translation = models.CharField(max_length=50)
+    status = models.CharField(max_length=20) # completed, not completed, viewed, not viewed
+    grammar = models.CharField(max_length=50) # verbs, nouns, adjectives, adverbs etc...
+    audio = 
+
+class category(models.Model):
+    name = models.CharField(max_length=50,primary_key=True)
+    description = models.JSONField()
+    translated_description = models.JSONField()
+
+
+    def __str__(self):
+        return self.word
 
