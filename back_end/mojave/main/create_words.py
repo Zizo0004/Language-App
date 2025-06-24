94c0c3f633494ebe8a854a026c074100ffcfc86f
ziyad <ziyad.alnawfal1@gmail.com>
2025-06-24 23:54:03 +0100

    completed database setup - 200 words with audio as well

--- back_end/mojave/main/create_words.py
+++ back_end/mojave/main/create_words.py
@@ -11,11 +11,10 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mojave.settings")
 django.setup()
 import pandas as pd
-import csv
+import csv,re
 from main import models
 from django.core.files.base import ContentFile
 from elevenlabs import ElevenLabs 
-
 from dotenv import load_dotenv
 import environ
 load_dotenv()
@@ -24,37 +23,47 @@ api = os.getenv("ELEVENLABS_API")
 
 
 database = pd.read_csv('C:/Users/Ziyad/Desktop/Backend/Language-App/back_end/mojave/main/database.csv')
-words = models.most_popular_words.objects.all()
-first_word = database.iloc[0]
-
-
-
-
 client = ElevenLabs(
     api_key=api, 
 )
 
-output = client.text_to_speech.convert(
-    voice_id="UgBBYS2sOqTuMpoF3BR0",
-    output_format="mp3_44100_128",
-    text="قَلِيل",
-    model_id="eleven_multilingual_v2",
-)
+def remove_harakat(text):
+    harakat_pattern = re.compile(r'[\u0610-\u061A\u064B-\u065F\u06D6-\u06DC\u06DF-\u06E8\u06EA-\u06ED]')
+    return harakat_pattern.sub('', text)
 
-for i in range(len(words)):
-audio = io.BytesIO()
 
-for audio_chunk in output:
-    if audio_chunk:
-        audio.write(audio_chunk)
+for i in range(len(database)):
+    words = models.most_popular_words.objects.all().values_list('word',flat=True)
+    current_word = database.iloc[i]
+    removed_harakat = remove_harakat(current_word['Word'])
+    word_exists = False
+    
+    for i in words:
+        if remove_harakat(i) == removed_harakat:
+            print(f"{current_word['Word']} already exists")
+            word_exists = True
+            break
+    if word_exists == False:
+            audio = io.BytesIO()
+            output = client.text_to_speech.convert(
+            voice_id="UgBBYS2sOqTuMpoF3BR0",
+            output_format="mp3_44100_128",
+            text=current_word['Word'],
+            model_id="eleven_multilingual_v2", 
+            )
 
-models.most_popular_words.objects.create(
-    word="قَلِيل",
-    translation="little",
-    answer="A",
-    status=True,
-    other="قَلِيل",
-    audio=ContentFile(audio.getvalue(), name="my_audio.mp3"),
-)
 
-print(models.most_popular_words.objects.all())
+            for audio_chunk in output:
+                if audio_chunk:
+                    audio.write(audio_chunk)
+            print(f"{current_word['Word']} created", i)
+
+            models.most_popular_words.objects.create(
+                word=current_word['Word'],
+                translation=current_word['Translation'],
+                status=False,
+                other=current_word['Other'],
+                audio=ContentFile(audio.getvalue(), name=f"{current_word['Word']}.mp3"), 
+            )
+
+
